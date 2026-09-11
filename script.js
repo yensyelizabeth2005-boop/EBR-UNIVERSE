2548f0c3b9a63d0cc7f1b1bf12325d8f3a7db646
yensyelizabeth2005-boop <yensyelizabeth2005@gmail.com>
2026-09-11 16:55:49 -0500

    Update script.js
    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 function startGame() {
-    alert("Welcome to EBR: UNIVERSE!");
+    document.body.innerHTML = `
+        <h1>EBR: UNIVERSE</h1>
+        <p>Welcome to your universe.</p>
+
+        <button onclick="createUniverse()">CREATE UNIVERSE</button>
+        <button>MY STORIES</button>
+        <button>SETTINGS</button>
+    `;
+}
+
+function createUniverse() {
+    document.body.innerHTML = `
+        <h1>Create Your Universe</h1>
+
+        <p>Universe Name</p>
+        <input type="text" id="universeName" placeholder="Enter a name">
+
+        <br><br>
+
+        <button onclick="saveUniverse()">CREATE</button>
+    `;
+}
+
+function saveUniverse() {
+    const name = document.getElementById("universeName").value;
+
+    if (name.trim() === "") {
+        alert("Please enter a universe name.");
+        return;
+    }
+
+    document.body.innerHTML = `
+        <h1>${name}</h1>
+        <p>Your universe has been created!</p>
+
+        <button>CREATE CHARACTER</button>
+    `;
 }
