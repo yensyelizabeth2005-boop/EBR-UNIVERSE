76b6fcf9e0cb98107a589f774fab49cc23f06f06
yensyelizabeth2005-boop <yensyelizabeth2005@gmail.com>
2026-09-11 20:04:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ function openPhone() {
         <p>Welcome to your character's phone.</p>
 
         <button onclick="openMessages()">💬 MESSAGES</button>
-       <button onclick="openContacts()">👥 CONTACTS</button>
+       <button onclick="alert('CONTACTS WORKS!')">👥 CONTACTS</button>
         <button>📸 PHOTOS</button>
         <button>🔔 NOTIFICATIONS</button>
     `;
